79d00fe07f8fb03c7398777029889efadaf1c232
Toffee.Albina <toffee.albina@tri.global>
2018-03-02 15:44:12 -0500

    publishing data (wrong data, but hey, publishing)

--- nodes/axis.py
+++ nodes/axis.py
@@ -3,8 +3,15 @@
 # /axis.py
 #
 
+import base64
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+
 import threading
-import urllib2
+import urllib.request
 
 ###import rospy 
 from sensor_msgs.msg import CompressedImage, CameraInfo
@@ -24,8 +31,7 @@ class StreamThread(threading.Thread):
     def stream(self):
         while(True):
             self.formURL()
-            self.authenticate()
-            if self.openURL():
+            if self.authenticateAndOpen():
                 self.publishFramesContinuously()
 ###            rospy.sleep(2) # if stream stays intact we shouldn't get to this
 
@@ -39,7 +45,7 @@ class StreamThread(threading.Thread):
         used this method (yet).'''
         if self.axis.password != '' and self.axis.username != '':
             # create a password manager
-            password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 
             # Add the username and password, use default realm.
             #top_level_url = "http://" + self.axis.hostname + self.axis.hostport
@@ -47,22 +53,42 @@ class StreamThread(threading.Thread):
             password_mgr.add_password(None, top_level_url, self.axis.username, 
                                                             self.axis.password)
             if self.axis.use_encrypted_password :
-                handler = urllib2.HTTPDigestAuthHandler(password_mgr)
+                handler = urllib.request.HTTPDigestAuthHandler(password_mgr)
             else:
-                handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+                handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
 
             # create "opener" (OpenerDirector instance)
-            opener = urllib2.build_opener(handler)
+            opener = urllib.request.build_opener(handler)
 
             # ...and install it globally so it can be used with urlopen.
-            urllib2.install_opener(opener)
+            urllib.request.install_opener(opener)
     
     def openURL(self):
         '''Open connection to Axis camera using http'''
         try:
-            self.fp = urllib2.urlopen(self.url, timeout=self.timeoutSeconds)
+            self.fp = urllib.request.urlopen(self.url, timeout=self.timeoutSeconds)
             return(True)
-        except urllib2.URLError, e:
+        except urllib.request.URLError as e:
+###            rospy.logwarn('Error opening URL %s Reason: %s. Looping until camera appears' % (self.url, e.reason))
+            # print e.reason
+            return(False)
+
+    def authenticateAndOpen(self):
+        '''only try to authenticate if user/pass configured.  I have not
+        used this method (yet).'''
+        if self.axis.password != '' and self.axis.username != '':
+            print("Creating req")
+            req = urllib.request.Request(self.url)
+            credentials = ("%s:%s" % (self.axis.username, self.axis.password))
+            encoded = base64.b64encode(credentials.encode("ascii"))
+            req.add_header("Authorization","Basic %s" % encoded.decode("ascii"))
+            
+        try:
+            self.fp = urllib.request.urlopen(req)
+            print("opening URL")
+            return(True)
+        except urllib.request.URLError as e:
+            print("error opening URL")
 ###            rospy.logwarn('Error opening URL %s Reason: %s. Looping until camera appears' % (self.url, e.reason))
             # print e.reason
             return(False)
@@ -74,17 +100,20 @@ class StreamThread(threading.Thread):
                 self.findBoundary()
                 self.getImage()
                 self.publishMsg()
-                self.publishCameraInfoMsg()
-            except:
+###                self.publishCameraInfoMsg()
+            except Exception as e:
+                print("Exception:", e)
 ###                rospy.loginfo('Timed out while trying to get message.')
                 break
 
     def findBoundary(self):
         '''The string "--myboundary" is used to denote the start of an image in
         Axis cameras'''
+        print ("finding boundary")
         while(True):
-            boundary = self.fp.readline()
+            boundary = self.fp.readline().decode("ascii")
             if boundary=='--myboundary\r\n':
+                print ("found a boundary")
                 break
 
     def getImage(self):
@@ -94,8 +123,9 @@ class StreamThread(threading.Thread):
 
     def getHeader(self):
         self.header = {}
+        print("getting header")
         while(True):
-            line = self.fp.readline()
+            line = self.fp.readline().decode("ascii")
             if line == "\r\n":
                 break
             line = line.strip()
@@ -103,10 +133,12 @@ class StreamThread(threading.Thread):
             try:
                 self.header[parts[0]] = parts[1]
             except:
+                print("Problem getting header")
 ###                rospy.logwarn('Problem encountered with image header.  Setting '
-                                                    'content_length to zero')
+###                                                    'content_length to zero')
                 self.header['Content-Length'] = 0 # set content_length to zero if 
                                             # there is a problem reading header
+        print(self.header)
         self.content_length = int(self.header['Content-Length'])
 
     def getImageData(self):
@@ -117,12 +149,17 @@ class StreamThread(threading.Thread):
 
     def publishMsg(self):
         '''Publish jpeg image as a ROS message'''
-        self.msg = CompressedImage()
+###        self.msg = CompressedImage()
+        self.msg = Image()
 ###        self.msg.header.stamp = rospy.Time.now()
         self.msg.header.frame_id = self.axis.frame_id
-        self.msg.format = "jpeg"
+###        self.msg.format = "jpeg"
+        self.msg.width = self.axis.width
+        self.msg.height = self.axis.height
+        self.msg.encoding = "bgr8"
         self.msg.data = self.img
-        self.axis.pub.publish(self.msg)
+        self.msg.step = int(len(self.msg.data)/self.msg.height)
+        self.axis.publisher_.publish(self.msg)
 
     def publishCameraInfoMsg(self):
         '''Publish camera info manager message'''
@@ -133,7 +170,7 @@ class StreamThread(threading.Thread):
         cimsg.height = self.axis.height
 ###        self.axis.caminfo_pub.publish(cimsg)
 
-class Axis:
+class Axis(Node):
     def __init__(self, hostname, hostport, username, password, width, height,
                  frame_id, camera_info_url, use_encrypted_password):
         self.hostname = hostname
@@ -146,14 +183,20 @@ class Axis:
         self.camera_info_url = camera_info_url
         self.use_encrypted_password = use_encrypted_password
         
+        super().__init__('axis_camera')
+
+    
         # generate a valid camera name based on the hostname
 ###        self.cname = camera_info_manager.genCameraName(self.hostname)
 ###        self.cinfo = camera_info_manager.CameraInfoManager(cname = self.cname,
 ###                                                   url = self.camera_info_url)
 ###        self.cinfo.loadCameraInfo()         # required before getCameraInfo()
         self.st = None
+        self.st = StreamThread(self)
+        self.st.start()
 ###        self.pub = rospy.Publisher("image_raw/compressed", CompressedImage, self, queue_size=1)
 ###        self.caminfo_pub = rospy.Publisher("camera_info", CameraInfo, self, queue_size=1)
+        self.publisher_ = self.create_publisher(Image, 'image')
 
     def __str__(self):
         """Return string representation."""
@@ -166,7 +209,6 @@ class Axis:
             self.st = StreamThread(self)
             self.st.start()
 
-
 def main():
 ###    rospy.init_node("axis_driver")
 ###    rospy.loginfo('axis_driver HELLO!')
@@ -175,15 +217,29 @@ def main():
         'hostname': '192.168.0.64',       # default IP address
         'hostport': '8082',               # default port
         'username': 'root',               # default login name
-        'password': '',
+        'password': 'drrobot',
         'width': 640,
         'height': 480,
         'frame_id': 'axis_camera',
         'camera_info_url': '',
         'use_encrypted_password' : False}
 ###    args = updateArgs(arg_defaults)
-    Axis(**args)
-###    rospy.spin()
+    args = updateArgsMini(arg_defaults)
+    rclpy.init(args=args)
+    axis_publisher = Axis(**args)
+    rclpy.spin(axis_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    axis_publisher.destroy_node()
+    rclpy.shutdown()
+    
+def updateArgsMini(arg_defaults):
+    args = {}
+    for name, val in arg_defaults.items():
+        args[name] = val
+    return(args)    
 
 def updateArgs(arg_defaults):
     '''Look up parameters starting in the driver's private parameter space, but
