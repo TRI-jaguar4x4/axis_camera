4258c9347c1200982953216dd1da67c04d73a63e
Toffee.Albina <toffee.albina@tri.global>
2018-03-06 11:00:29 -0500

    convert jpeg to raw before publishing

--- nodes/axis.py
+++ nodes/axis.py
@@ -5,6 +5,9 @@
 
 import base64
 
+import PIL
+from PIL import Image as PILImage
+
 import rclpy
 from rclpy.node import Node
 
@@ -40,44 +43,10 @@ class StreamThread(threading.Thread):
         self.url += "?fps=0&resolution=%dx%d" % (self.axis.width, self.axis.height)
 ###        rospy.loginfo('opening ' + str(self.axis))
 
-    def authenticate(self):
-        '''only try to authenticate if user/pass configured.  I have not
-        used this method (yet).'''
-        if self.axis.password != '' and self.axis.username != '':
-            # create a password manager
-            password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-
-            # Add the username and password, use default realm.
-            #top_level_url = "http://" + self.axis.hostname + self.axis.hostport
-            top_level_url = self.url
-            password_mgr.add_password(None, top_level_url, self.axis.username, 
-                                                            self.axis.password)
-            if self.axis.use_encrypted_password :
-                handler = urllib.request.HTTPDigestAuthHandler(password_mgr)
-            else:
-                handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-
-            # create "opener" (OpenerDirector instance)
-            opener = urllib.request.build_opener(handler)
-
-            # ...and install it globally so it can be used with urlopen.
-            urllib.request.install_opener(opener)
-    
-    def openURL(self):
-        '''Open connection to Axis camera using http'''
-        try:
-            self.fp = urllib.request.urlopen(self.url, timeout=self.timeoutSeconds)
-            return(True)
-        except urllib.request.URLError as e:
-###            rospy.logwarn('Error opening URL %s Reason: %s. Looping until camera appears' % (self.url, e.reason))
-            # print e.reason
-            return(False)
-
     def authenticateAndOpen(self):
-        '''only try to authenticate if user/pass configured.  I have not
-        used this method (yet).'''
+        '''only try to authenticate if user/pass configured,
+        open connection to Axis camera using http.'''
         if self.axis.password != '' and self.axis.username != '':
-            print("Creating req")
             req = urllib.request.Request(self.url)
             credentials = ("%s:%s" % (self.axis.username, self.axis.password))
             encoded = base64.b64encode(credentials.encode("ascii"))
@@ -85,12 +54,10 @@ class StreamThread(threading.Thread):
             
         try:
             self.fp = urllib.request.urlopen(req)
-            print("opening URL")
             return(True)
         except urllib.request.URLError as e:
-            print("error opening URL")
 ###            rospy.logwarn('Error opening URL %s Reason: %s. Looping until camera appears' % (self.url, e.reason))
-            # print e.reason
+            print ("Exception opening URL: ", e.reason)
             return(False)
 
     def publishFramesContinuously(self):
@@ -102,18 +69,16 @@ class StreamThread(threading.Thread):
                 self.publishMsg()
 ###                self.publishCameraInfoMsg()
             except Exception as e:
-                print("Exception:", e)
+                print("Exception publishing: ", e.reason)
 ###                rospy.loginfo('Timed out while trying to get message.')
                 break
 
     def findBoundary(self):
         '''The string "--myboundary" is used to denote the start of an image in
         Axis cameras'''
-        print ("finding boundary")
         while(True):
             boundary = self.fp.readline().decode("ascii")
             if boundary=='--myboundary\r\n':
-                print ("found a boundary")
                 break
 
     def getImage(self):
@@ -123,7 +88,6 @@ class StreamThread(threading.Thread):
 
     def getHeader(self):
         self.header = {}
-        print("getting header")
         while(True):
             line = self.fp.readline().decode("ascii")
             if line == "\r\n":
@@ -132,8 +96,8 @@ class StreamThread(threading.Thread):
             parts = line.split(": ", 1)
             try:
                 self.header[parts[0]] = parts[1]
-            except:
-                print("Problem getting header")
+            except Exception as e:
+                print("Exception getting header: ", e.reason)
 ###                rospy.logwarn('Problem encountered with image header.  Setting '
 ###                                                    'content_length to zero')
                 self.header['Content-Length'] = 0 # set content_length to zero if 
@@ -151,15 +115,27 @@ class StreamThread(threading.Thread):
         '''Publish jpeg image as a ROS message'''
 ###        self.msg = CompressedImage()
         self.msg = Image()
+
 ###        self.msg.header.stamp = rospy.Time.now()
-        self.msg.header.frame_id = self.axis.frame_id
 ###        self.msg.format = "jpeg"
+        self.msg.header.frame_id = self.axis.frame_id
         self.msg.width = self.axis.width
         self.msg.height = self.axis.height
-        self.msg.encoding = "bgr8"
-        self.msg.data = self.img
+        self.msg.encoding = "rgb8"
         self.msg.step = int(len(self.msg.data)/self.msg.height)
+
+        try:
+            im = PILImage.frombuffer("RGB",
+                                     (self.msg.width,self.msg.height),
+                                     self.img, "jpeg", "RGB", "")
+        except Exception as e:
+            print ("Exception loading PILImage.frombuffer: ", e)
+
+###        self.msg.data = self.img
+        self.msg.data = im.tobytes()
         self.axis.publisher_.publish(self.msg)
+
+
 
     def publishCameraInfoMsg(self):
         '''Publish camera info manager message'''
@@ -182,7 +158,7 @@ class Axis(Node):
         self.frame_id = frame_id
         self.camera_info_url = camera_info_url
         self.use_encrypted_password = use_encrypted_password
-        
+
         super().__init__('axis_camera')
 
     
